a962c4c7fa9ca0afba8f31634e52f90a6e6a4d03
cjsrn1940 <cjsrn1940@gmail.com>
2021-05-28 10:11:23 +0900

    5/28

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -9,17 +9,17 @@ public class Ex10 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 입력하세요");
 		int num;
-		int sum = 0;
+		int max = 0;
 		
 		for(int i = 0; i < 5; i++) {
 			System.out.print("숫자 : ");
 			num = sc.nextInt();
-			if(num > sum) {
-				sum = num;
+			if(num > max) {
+				max = num;
 			}
 		}
 		
-		System.out.println("최대값은 " + sum + " 입니다.");
+		System.out.println("최대값은 " + max + " 입니다.");
 		
 		sc.close();
 		
